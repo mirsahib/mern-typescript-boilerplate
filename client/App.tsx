bc14d87bffe93bc1f5b5ba0a7191b2f494f93ecd
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-10-11 19:02:19 +0600

    :tada: Create client folder

---
+++ client/App.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+import {hot} from 'react-hot-loader'
+
+const App:React.FC = ():ReactElement=>{
+    return(
+        <>App</>
+    )
+}
+export default hot(module)(App)
