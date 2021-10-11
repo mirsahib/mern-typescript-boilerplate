fb302e2e8061cbe02426bf16e81248654e484390
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-10-11 19:05:08 +0600

    :wrench: Add webpack configuration for client

---
+++ webpack.config.client.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+  entry: {main:'./client/main.tsx'},
+  devtool:'inline-source-map',
+  target:'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude:path.resolve(__dirname, "node_modules")
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx','js']
+  },
+  output: {
+    filename: 'client.generated.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  externals: [ nodeExternals() ]
+};
